6e1d31f3019370a100f808a8ece5f7198ec5197b
Egor <bugrov102002@yandex.ru>
2022-05-25 18:14:58 +0300

    Javadoc.

---
+++ Docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"comand.play.shootemup"},{"l":"comand.play.shootemup.controller"},{"l":"comand.play.shootemup.model"},{"l":"comand.play.shootemup.view"}]
